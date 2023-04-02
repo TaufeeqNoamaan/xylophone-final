0a1fb0b09cda0995a5fe38d6176b786a7d58d572
TaufeeqNoamaan <89037322+TaufeeqNoamaan@users.noreply.github.com>
2023-04-02 12:46:10 +0530

    Completed app

--- README.md
+++ README.md
@@ -1,16 +1,9 @@
 # xylophone_app
 
-A new Flutter project.
+This is the Completed Version of the Xylophone app
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+Just run the app and play your symphonies
 
-A few resources to get you started if this is your first Flutter project:
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
---
+++ lib/home.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List colors = [
+      Colors.red,
+      Colors.orange,
+      Colors.yellow,
+      Colors.green,
+      Colors.teal,
+      Colors.blue,
+      Colors.purple
+    ];
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          'Play your tune',
+          style: GoogleFonts.pacifico(
+              color: Colors.white, fontSize: 30, fontWeight: FontWeight.w400),
+        ),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          SoundButton(
+            color: colors[0],
+            num: "1",
+          ),
+          SoundButton(
+            color: colors[1],
+            num: "2",
+          ),
+          SoundButton(
+            color: colors[2],
+            num: "3",
+          ),
+          SoundButton(
+            color: colors[3],
+            num: "4",
+          ),
+          SoundButton(
+            color: colors[4],
+            num: "5",
+          ),
+          SoundButton(
+            color: colors[5],
+            num: "6",
+          ),
+          SoundButton(
+            color: colors[6],
+            num: "7",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SoundButton extends StatelessWidget {
+  final Color color;
+  final num;
+  const SoundButton({
+    super.key,
+    required this.color,
+    this.num,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SizedBox(
+        width: double.infinity,
+        height: 65,
+        child: ElevatedButton(
+            style:
+                ButtonStyle(backgroundColor: MaterialStateProperty.all(color)),
+            onPressed: () async {
+              // final player = AudioPlayer();
+
+              // await player
+              //     .setSource(AssetSource('assets/assets_note1.wav'));
+              final assetsAudioPlayer = AssetsAudioPlayer();
+
+              assetsAudioPlayer.open(
+                Audio("assets/assets_note$num.wav"),
+              );
+            },
+            child: null),
+      ),
+    );
+  }
+}
